9dd80e7d7d78074eb8c443ac2e2552418d747b0d
kenwilcox <wilcoxk@gmail.com>
2015-01-02 14:07:20 -0700

    setup action and outlets

--- iOS8SwiftUIStepper/ViewController.swift
+++ iOS8SwiftUIStepper/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var valueLabel: UILabel!
+  @IBOutlet weak var stepper: UIStepper!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,7 @@ class ViewController: UIViewController {
   }
 
 
+  @IBAction func stepperValueChanged(sender: UIStepper) {
+  }
 }
 
