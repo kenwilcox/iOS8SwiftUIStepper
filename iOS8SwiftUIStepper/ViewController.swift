971f13be0e09e06495ca84a4c65d87d17c55dd79
kenwilcox <wilcoxk@gmail.com>
2015-01-02 14:09:11 -0700

    setup stepper properties

--- iOS8SwiftUIStepper/ViewController.swift
+++ iOS8SwiftUIStepper/ViewController.swift
@@ -15,7 +15,10 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    stepper.wraps = true
+    stepper.autorepeat = true
+    stepper.maximumValue = 10
   }
 
   override func didReceiveMemoryWarning() {
